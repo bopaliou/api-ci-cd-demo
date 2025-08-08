b72990c752c3e93ae741c70675ea5172c5e3ced6
Aliou Bop <bopaliou1994@gmail.com>
2025-08-08 14:42:08 +0000

    Add env support and render.yaml for auto-deployment

--- api_ci_cd_demo/settings.py
+++ api_ci_cd_demo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k14_zqr4b-#7_f94z1^_^vw5u1=yvd#b-+xr9j5q$h9_*5x!rh'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
