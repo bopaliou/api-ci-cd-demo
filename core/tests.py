8ab1483ae0a805b8ceb50cfea7e03354b054f285
Aliou Bop <bopaliou1994@gmail.com>
2025-08-08 13:44:43 +0000

    Test passed: fix ping test path

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,9 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-class PingTestCase(TestCase):
+class PingTestCase(SimpleTestCase):
     def test_ping(self):
-        response =self.client.get('/ping/')
+        response = self.client.get('/api/ping/')
+        print("Response code:", response.status_code)
+        print("Response content:", response.content)
         self.assertEqual(response.status_code, 200)
